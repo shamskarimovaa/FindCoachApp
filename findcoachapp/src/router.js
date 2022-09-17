49781e4bfdf9811622bc25297b83476170b8ceb9
shamskarimovaa <shamskarimova2000s@gmail.com>
2022-09-17 13:01:09 +0400

    register pages

--- findcoachapp/src/router.js
+++ findcoachapp/src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachRegister from './pages/coaches/CoachRegister.vue';
+import CoachRequest from './pages/coaches/CoachRequest.vue';
+import RequestsList from './pages/coaches/RequestsList.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachesList },
     {
       path: "/coached/:id",
-      component: null,
-      children: [{ path: "contact", component: null }],
+      component: CoachDetail,
+      children: [{ path: "contact", component: CoachRequest }],
     },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: CoachRegister },
+    { path: "/requests", component: RequestsList },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
