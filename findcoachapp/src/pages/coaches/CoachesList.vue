a0a9ec4f5f84a379f8f1e7fbd74a43783fe271fe
shamskarimovaa <shamskarimova2000s@gmail.com>
2022-09-17 15:12:07 +0400

    add vuex

--- findcoachapp/src/pages/coaches/CoachesList.vue
+++ findcoachapp/src/pages/coaches/CoachesList.vue
@@ -1,10 +1,28 @@
 <template>
-
   <section>FILTER</section>
   <section>
     <div>
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    LIST OF COACHES</section>
+    LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in coaches" :key="coach.id">
+      {{coach.firstName}}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found</h3>
+  </section>
 </template>
+<script>
+export default {
+  computed: {
+    coaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
