3dcee13d237262336276462f9e4dac4259d0e41a
shamskarimovaa <shamskarimova2000s@gmail.com>
2022-09-17 13:04:36 +0400

    add App.vue

--- findcoachapp/src/router.js
+++ findcoachapp/src/router.js
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegister from './pages/coaches/CoachRegister.vue';
-import CoachRequest from './pages/coaches/CoachRequest.vue';
-import RequestsList from './pages/coaches/RequestsList.vue';
+import CoachRequest from './pages/requests/CoachRequest.vue';
+import RequestsList from './pages/requests/RequestsList.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
