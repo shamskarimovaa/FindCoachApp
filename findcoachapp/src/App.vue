7b82bb80da9c9c7eed7ac4b8a297965defbd0fe6
shamskarimovaa <shamskarimova2000s@gmail.com>
2022-09-17 13:44:13 +0400

    add navbar and styling

--- findcoachapp/src/App.vue
+++ findcoachapp/src/App.vue
@@ -1,3 +1,26 @@
 <template>
-    <router-view></router-view>
+  <nav-bar></nav-bar>
+  <router-view></router-view>
 </template>
+
+<script>
+import NavBar from "./components/layout/NavBar.vue";
+export default {
+    components: {NavBar},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+*{
+    box-sizing: border-box;
+}
+
+html{
+    font-family: "Roboto", sans-serif;
+}
+
+body{
+    margin: 0;
+}
+</style>
